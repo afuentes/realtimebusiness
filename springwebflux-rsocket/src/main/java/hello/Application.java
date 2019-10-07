85c18a9ea193bb6dbbc5902fb903b76686ce1b76
alexfuentes <fuentes.alex@gmail.com>
2019-10-07 16:15:14 -0300

    updated

--- springwebflux-rsocket/src/main/java/hello/Application.java
+++ springwebflux-rsocket/src/main/java/hello/Application.java
@@ -1,15 +1,16 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-
-		GreetingWebClient gwc = new GreetingWebClient();
-		System.out.println(gwc.getResult());
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .main(Application.class)
+                .sources(Application.class)
+                .profiles("server")
+                .run(args);
+    }
 }
