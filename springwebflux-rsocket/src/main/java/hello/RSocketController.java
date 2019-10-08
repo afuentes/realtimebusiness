e71da5fe39ccc2a707e2d8f99d817c3bcf2b7023
alexfuentes <fuentes.alex@gmail.com>
2019-10-08 02:15:00 -0300

    updated

--- springwebflux-rsocket/src/main/java/hello/RSocketController.java
+++ springwebflux-rsocket/src/main/java/hello/RSocketController.java
@@ -1,21 +1,21 @@
 package hello;
 
-import java.time.Duration;
-
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
+import lombok.Data;
 import reactor.core.publisher.Mono;
 
 @Controller
-public class RSocketController {
-
-    @MessageMapping("/health")
-    public Mono<Void> handleHealth(String payload) {
-        return Mono.delay(Duration.ofMillis(10))
-        .map(l -> createResponse(payload));
-
+@MessageMapping("hello")
+class GreetingServerController {   
+    public Mono<Void> hello(Greeting p) {
+        return Mono.empty();
     }
+    
+}
 
-
+@Data
+class Greeting {
+    String name;
 }
